02a0c7be93b8c281cbda385ac0d6e4c17fb8477e
JJ <kongpopsattarach.edu@gmail.com>
2023-12-01 15:36:59 +0700

    finish Q1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x;
+    int even=0,odd=0;
+    while (x!=0)
+    {
+        cout << "Enter an integer: ";
+        cin >> x;
+        if (x%2==0)
+        {
+            even ++;
+        }else
+        {
+            odd ++;
+        }
+    }
+    
+    cout << "#Even numbers = " << even-1 << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
